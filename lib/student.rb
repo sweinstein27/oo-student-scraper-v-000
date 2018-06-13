091879da78124aa14b875c9e60f25f4e909dd629
Scott Weinstein <sweinstein27@gmail.com>
2018-06-13 16:00:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,8 @@ class Student
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def initialize(students)
+    students.each_pair{ |key, value| self.send(("#{key}="), value)} 
   end
 
   def self.create_from_collection(students_array)
